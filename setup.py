b04f2c93349e75843018630800730066b5910610
Aijaz <aijaz523@gmail.com>
2024-07-28 16:37:03 +0530

    updated with ci and releaase code

--- setup.py
+++ setup.py
@@ -20,27 +20,32 @@ with open("README.md", 'r', encoding='utf-8') as f:
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
+    try:
+        with open(file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
+            print(requirements)
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+    except FileNotFoundError:
+        print(f"Warning: {file_path} not found.")
+        return []
     return requirements
 
 
-setup(
-    name=PKG_NAME,
-    version='0.0.1',
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description=pkg_desc,
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=url,
-    project_urls=project_urls,
-    package_dir=package_dir,
-    packages=find_packages(where="src"),
-    install_requires=get_requirements("requirements_dev.txt")
-)
+# setup(
+#     name=PKG_NAME,
+#     version='0.0.1',
+#     author=AUTHOR_USER_NAME,
+#     author_email=AUTHOR_EMAIL,
+#     description=pkg_desc,
+#     long_description=long_description,
+#     long_description_content="text/markdown",
+#     url=url,
+#     project_urls=project_urls,
+#     package_dir=package_dir,
+#     packages=find_packages(where="src"),
+#     install_requires=get_requirements("requirements_dev.txt")
+# )
+if __name__ == "__main__":
+    get_requirements("requirements_dev.txt")
---
+++ test_setup.py
@@ -0,0 +1,2 @@
+from setup import get_requirements
+get_requirements("./requirements_dev.txt")
